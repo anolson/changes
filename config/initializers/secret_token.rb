804fdfb5759ece96f48a3a660768814934575a77
Andrew Olson <aolson@geezeo.com>
2014-01-02 21:41:09 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniPr::Application.config.secret_key_base = 'd655eb932f8b53acf166ced1c0ded716911a54523c3391d1a09124e17d80f211c50407826df2881cf1e8e5f24feafa6618cab202ee1ed7cb056a431cecbf3d4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MiniPr::Application.config.session_store :cookie_store, key: '_mini_pr_session'
