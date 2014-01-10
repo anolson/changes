c88920677e895565b748b135e816e7b48b5c6e6b
Andrew Olson <aolson@geezeo.com>
2014-01-09 21:28:36 -0500

    Move total_additions and total_deletions into decorator.

--- app/decorators/pull_request_decorator.rb
+++ app/decorators/pull_request_decorator.rb
@@ -4,4 +4,12 @@ class PullRequestDecorator < Draper::Decorator
   def diff_files
     @diff_files ||= GitDiff.from_string(diff)
   end
+
+  def total_additions
+    diff_files.map(&:total_additions).inject(:+)
+  end
+
+  def total_deletions
+    diff_files.map(&:total_additions).inject(:+)
+  end
 end
