7bf1f81596a4f7d86318c8512f9b16484f4f877e
Andrew Olson <aolson@geezeo.com>
2014-02-03 20:16:06 -0500

    Add index page and update style.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Changes::Application.routes.draw do
   resources :patches, only: [:show, :create]
+  resources :pages, only: [:index]
+
   get '/:permalink', to: 'patches#show'
+  root to: 'pages#index'
 end
