30afb1b49c19e5f0d0422f5089143f215136fb92
Andrew Olson <aolson@geezeo.com>
2014-02-03 20:29:33 -0500

    sqlite -> postgresql.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140126022021) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "patches", force: true do |t|
     t.text     "raw"
     t.datetime "created_at"
